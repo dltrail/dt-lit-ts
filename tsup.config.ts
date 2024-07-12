62f1c6c04c4bb8de60f07d8974d928a4f699bb9d
Dawn Trail <dawn.trail@thetin.net>
2024-07-12 15:15:07 +0100

    instal tsup and web dev server

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
+  outDir: 'dist',
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
+  minify: true,
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
   sourcemap: true,
   clean: true,
+  env: {
+    NODE_ENV: 'production', // set environment on build
+  }
 });
